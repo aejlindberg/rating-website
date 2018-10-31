a445eca120c2f45b2018d77f44a4dfc2519761a0
A-Game <javascript@adamheldring.com>
2018-10-31 12:17:13 +0100

    Created backend endpoints

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 
 app.use(cors())
 
-const mongoServer = "mongo://localhost/myNewDB"
+const mongoServer = "mongodb://localhost/sprint5DB"
 
 mongoose.connect(mongoServer, { useNewUrlParser: true })
 mongoose.Promise = Promise
@@ -18,7 +18,7 @@ mongoose.connection.on("error", err => console.error("Connection error:", err))
 mongoose.connection.once("open", () => console.log("Connected to mongodb"))
 
 const Product = mongoose.model("Product", {
-  itemID: Number,
+  productID: Number,
   sellerID: String,
   title: String,
   description: String,
@@ -32,4 +32,30 @@ app.get("/", (req, res) => {
   res.send("Team 1 API")
 })
 
-app.listen(8080, () => console.log("Team 1 API listening on port 8080!"))
+app.get("/products/", (req, res) => {
+  Product.find().then(products => {
+    res.json(products)
+  })
+})
+
+app.post("/products/", (req, res) => {
+  const product = new Product(req.body)
+  console.log("Body: ", req.body)
+
+  product.save()
+    .then(() => { res.status(201).send("Product created") })
+    .catch(err => { res.status(400).send(err) })
+})
+
+app.put("/products/:productId", (req, res) => {
+  // const product = new Product(req.body)
+  // console.log("Body: ", req.body)
+  //
+  // product.save()
+  //   .then(() => { res.status(201).send("Product created") })
+  //   .catch(err => { res.status(400).send(err) })
+})
+
+
+
+app.listen(8081, () => console.log("Team 1 API listening on port 8081!"))
