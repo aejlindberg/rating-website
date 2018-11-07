a20e6a3380da2c536f76b8e19792d516abad13f0
Jeanette <aejlindberg@gmail.com>
2018-11-07 14:54:44 +0100

    added icons to categories

--- frontend/src/components/singleProduct/singleProduct.js
+++ frontend/src/components/singleProduct/singleProduct.js
@@ -4,33 +4,53 @@ import RatingCounter from "../RatingCounter/RatingCounter"
 
 class SingleProduct extends React.Component {
 
-  render() {
-    return (
-      <div className="singleProductContainer">
-        <img className="productImage" src={this.props.image} />
-        <div className="singleProductInfoContainer">
-          <div className="productText">
-            <h3>{this.props.title}</h3>
-            <p>{this.props.description}</p>
+  checkIcon = () => {
+    console.log("test")
+    let image = ""
+    switch (this.props.category) {
+      case "clothing":
+        image = "./clothes.svg"
+        break
+      case "shoes":
+        image = "./shoes.svg"
+        break
+      default:
+        image = "./music.svg"
+    }
+    return image
+  }
+
+render() {
+
+  return (
+    <div className="singleProductContainer">
+      <img className="productImage" src={this.props.image} />
+            <div className="singleProductInfoContainer">
+        <div className="productText">
+          <h3>{this.props.title}</h3>
+          <p>{this.props.description}</p>
+      <div className="singleProductCategory">
+            <img src={this.checkIcon()} alt="category" />
             <p>Category: {this.props.category}</p>
           </div>
-          <div className="singleProductDataContainer">
-            <div className="productPrice">
-              <span>$ {this.props.price}</span>
-            </div>
-            <div className="productRating">
-              <RatingCounter
-                changeRating={(ratingCounter_index, delta) => this.props.changeRating(ratingCounter_index, delta)}
-                index={this.props.productIndex}
-                rating={this.props.rating} />
-                <p>Number of votes: {this.props.rating}</p>
-            </div>
+        </div>
+        <div className="singleProductDataContainer">
+          <div className="productPrice">
+            <span>$ {this.props.price}</span>
+          </div>
+          <div className="productRating">
+            <RatingCounter
+              changeRating={(ratingCounter_index, delta) => this.props.changeRating(ratingCounter_index, delta)}
+              index={this.props.productIndex}
+              rating={this.props.rating} />
+              <p>Number of votes: {this.props.rating}</p>
           </div>
         </div>
       </div>
-    )
-  }
-
+    </div>
+  )
 }
+}
+
 
 export default SingleProduct
