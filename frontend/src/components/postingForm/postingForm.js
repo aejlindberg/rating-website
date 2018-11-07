b67e952f36842b70345b7a3bfe865e0e528e893c
A-Game <javascript@adamheldring.com>
2018-11-07 13:56:19 +0100

    Statically assigned rating and votes - no more input fields

--- frontend/src/components/postingForm/postingForm.js
+++ frontend/src/components/postingForm/postingForm.js
@@ -9,6 +9,7 @@ class PostingForm extends React.Component {
         <h2>Post your item</h2>
         <form id="postingForm" className="postingForm" method="post" action="http://localhost:8081/products">
           <input className="formInput" type="text" placeholder="Title" name="title" required />
+          <input className="formInput" type="email" placeholder="E-mail" name="email" required />
           <textarea className="formInput" rows="4" cols="50" placeholder="Add a description" name="description" form="postingForm" required />
           <input className="formInput" type="number" placeholder="Price" name="price" required />
           <input className="formInput" type="file" placeholder="Image" name="image" required />
@@ -18,8 +19,6 @@ class PostingForm extends React.Component {
             <option value="music">Music</option>
             <option value="bears">Bears</option>
           </select>
-          <input type="number" placeholder="Rating" name="rating" required />
-          <input type="number" placeholder="Rating" name="nrOfVotes" value="0" required />
           <input className="submitButton" type="submit" />
         </form>
       </div>
