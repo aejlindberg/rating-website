718d031d5a3bcbef1f2960aa63354fc3a6f8ad69
Linda Olander <lindaolander@users.noreply.github.com>
2018-11-07 10:02:53 +0100

    Added thumbs to RatingCounter

--- frontend/src/components/RatingCounter/RatingCounter.js
+++ frontend/src/components/RatingCounter/RatingCounter.js
@@ -6,9 +6,13 @@ const RatingCounter =({index, rating, changeRating}) => {
 
   return (
     <div className="counter">
-      <button className="counter-action decrement" onClick ={()=> changeRating(index, -1)}> - </button>
+      <button className="counter-action decrement" onClick ={()=> changeRating(index, +1)}>
+        <img src="./thumbs_up.png" alt="minus" />
+      </button>
       <span className="counter-rating"> {rating} </span>
-      <button className="counter-action increment" onClick ={()=> changeRating(index, +1)}> + </button>
+      <button className="counter-action increment" onClick ={()=> changeRating(index, -1)}>
+        <img src="./thumbs_down.png" alt="minus" />
+      </button>
     </div>
   );
 }
--- frontend/src/components/singleProduct/singleProduct.js
+++ frontend/src/components/singleProduct/singleProduct.js
@@ -13,7 +13,7 @@ class SingleProduct extends React.Component {
         <p>{this.props.description}</p>
         <p>Category: {this.props.category}</p>
         <span>$ {this.props.price}</span>
-        <p>{this.props.rating}</p>
+        <p>Rating: {this.props.rating}</p>
         <RatingCounter
           changeRating={(ratingCounter_index, delta) => this.props.changeRating(ratingCounter_index, delta)}
           index={this.props.productIndex}
