122c844605cf07dc714df211c44edc1ff12a61c9
thaiyue <thaiyu@gmail.com>
2018-11-08 12:24:58 +0100

    Added dummy info

--- backend/server.js
+++ backend/server.js
@@ -67,6 +67,32 @@ app.put("/products/:objectID", (req, res) => {
   })
 })
 
+// const products = [
+//   new Product({ title: "Ringo's Glasses", email: "ringo@beatles.com", description: "Ringo's favorite glasses circa 2009", price: 43, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Lennon's Sneakers", email: "koko@beatles.com", description: "Jon's running shoes", price: 60, image:"", category:"shoes", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Paul's Jacket", email: "paul@beatles.com", description: "Paul's jacket worn during Woodstock", price: 25, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "George's Watch", email: "harrison@beatles.com", description: "George's despicably charming watch", price: 231, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Koko's Scarf", email: "kok@beatles.com", description: "Do you really care?", price: 1, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Ozzy's leather pants", email: "crazytrain@osborne.com", description: "Beer encrusted leather pants", price: 342, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Riahanna's umbrella", email: "rhianna@rhianna.com", description: "The umbrella from the music video", price: 209, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Running out of titles", email: "ringo@beatles.com", description: "No idea what should go here at this point", price: 3234, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Mick's lip gloss", email: "jagger@stones.com", description: "Jagger's strawberry lip balm", price: 1, image:"", category:"shoes", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Keith's favorite bottle of jack", email: "keith@stones.com", description: "Keith's smuggled bottle of whiskey out of Tennessee", price: 34, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Ronnie's sax reed", email: "ronnie@stones.com", description: "Ronnie's favorite set of reeds", price: 5, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Charlie's favorite brass knuckles", email: "watts@stones.com", description: "The knuckles that Charlie wish he had when knocking Jagger out", price: 23, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Wyman's funny looking gloves", email: "wyman@stones.com", description: "Wyman's gloves. Nuff said. ", price: 4534, image:"", category:"bears", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "The Who's collective socks from 1982", email: "who@who.com", description: "Who's socks from the who your", price: 34, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "The Door's collective doors from 1976", email: "door@doors.com", description: "Why would you need this?", price: 453, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Elton John's piano cover", email: "elton@john.com", description: "Elton John's piano cover from his childhood", price: 43, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Some really interesting thing", email: "ringo@beatles.com", description: "Fast description", price: 453421, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "2pac's chain necklace", email: "2pac@2pac.com", description: "California love", price: 432, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//   new Product({ title: "Eminem's track suit", email: "marshal@mathers.com", description: "2 trailer park girls go round the outside", price: 3243, image:"", category:"clothing", rating:0, nrOfVotes: 0 }),
+//
+// ]
+//
+// products.forEach(product => {
+//   product.save().then(() => { console.log("Created", product.title )})
+// })
 
 
 app.listen(8081, () => console.log("Team 1 API listening on port 8081!"))
--- frontend/src/components/postingForm/postingForm.js
+++ frontend/src/components/postingForm/postingForm.js
@@ -2,18 +2,70 @@ import React from "react"
 import "./postingForm.scss"
 
 class PostingForm extends React.Component {
+state = {
+  title: "",
+  email: "",
+  description: "",
+  price: "",
+  image: "",
+  category: "clothing"
+}
+
+handleFormChange= (e)=> {
+  console.log(e.target.value)
+  this.setState({
+    [e.target.name]: e.target.value
+  })
+}
+clearFields= () => {
+  this.setState ({
+    title: "",
+    email: "",
+    description: "",
+    price: "",
+    image: "",
+    category: "clothing"
+  })
+}
+
+submitForm = (e) => {
+  e.preventDefault()
+  const newProduct= {
+    title: this.state.title,
+    email: this.state.email,
+    description: this.state.description,
+    price: this.state.price,
+    image: this.state.image,
+    category: this.state.category
+  }
+  fetch("http://localhost:8081/products", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "Post",
+    body: JSON.stringify(newProduct)
+  })
+  .then(data => {
+    console.log('Request success: ', data)
+  })
+  .catch(error => {
+    console.log('Request failure: ', error)
+  })
+  alert( this.state.title + " has been created!")
+  this.clearFields()
+}
 
   render() {
     return (
       <div className="postingFormContainer">
         <h2>Post your item</h2>
-        <form id="postingForm" className="postingForm" method="post" action="http://localhost:8081/products">
-          <input className="formInput" type="text" placeholder="Title" name="title" required />
-          <input className="formInput" type="email" placeholder="E-mail" name="email" required />
-          <textarea className="formInput" rows="4" cols="50" placeholder="Add a description" name="description" form="postingForm" required />
-          <input className="formInput" type="number" placeholder="Price" name="price" required />
-          <input className="formInput" type="url" placeholder="Image" name="image" required />
-          <select className="formInput" name="category" size="1">
+        <form id="postingForm" className="postingForm" onSubmit={this.submitForm}>
+          <input className="formInput" value={this.state.title} type="text" placeholder="Title" name="title" required onChange={this.handleFormChange}/>
+          <input className="formInput" value={this.state.email} type="email" placeholder="E-mail" name="email" required onChange={this.handleFormChange}/>
+          <textarea className="formInput" value={this.state.description} rows="4" cols="50" placeholder="Add a description" name="description" form="postingForm" required onChange={this.handleFormChange}/>
+          <input className="formInput" value={this.state.price} type="number" placeholder="Price" name="price" required onChange={this.handleFormChange}/>
+          <input className="formInput" value={this.state.image} type="url" placeholder="Image" name="image" required onChange={this.handleFormChange}/>
+          <select className="formInput" name="category" size="1" onChange={this.handleFormChange}>
             <option value="clothing">Clothing</option>
             <option value="shoes">Shoes</option>
             <option value="music">Music</option>
